c4736856ddfef7208c39b4a64d67a89de70f849f
yourirucunov123-dev <youri.rucunov.123@gmail.com>
2026-01-12 21:59:18 +0500

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDdOZrTcE4ATDlGldnW8_dWRmHuaUuJfak",
+    authDomain: "mysuperchat-d1c29.firebaseapp.com",
+    projectId: "mysuperchat-d1c29",
+    storageBucket: "mysuperchat-d1c29.firebasestorage.app",
+    messagingSenderId: "299888628640",
+    appId: "1:299888628640:web:caf0535b94e389c7948ac5"
+};
+
+export const IMGBB_KEY = "699042c759021469e38f5370217a808f";
